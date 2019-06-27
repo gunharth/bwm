4d6cbfa9631b6dacd8492b648863d784e12aabc7
Gunharth Randolf <hello@gunharth.io>
2019-06-27 11:07:44 +0200

    Add router page transition to fade

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,7 @@
 import Home from "@/components/Home";
 import Snackbar from "@/components/Snackbar";
 import Toolbar from "@/components/Toolbar";
-import firebase from "@/firebaseConfig.js";
-import axios from "axios";
-//import { firestore } from "firebase";
-const { messaging } = firebase;
+
 export default {
   name: "App",
   components: {
@@ -27,80 +24,20 @@ export default {
   },
   data() {
     return {};
-  },
-  mounted() {
-    // this.listenTokenRefresh();
-  },
-  methods: {
-    // getMessagingToken() {
-    //   messaging
-    //     .getToken()
-    //     .then(async token => {
-    //       if (token) {
-    //         const currentMessageToken = window.localStorage.getItem(
-    //           "messagingToken"
-    //         );
-    //         console.log("token will be updated", currentMessageToken != token);
-    //         if (currentMessageToken != token) {
-    //           await this.saveToken(token);
-    //         }
-    //       } else {
-    //         console.log(
-    //           "No Instance ID token available. Request permission to generate one."
-    //         );
-    //         this.notificationsPermisionRequest();
-    //       }
-    //     })
-    //     .catch(function(err) {
-    //       console.log("An error occurred while retrieving token. ", err);
-    //     });
-    // },
-    // notificationsPermisionRequest() {
-    //   messaging
-    //     .requestPermission()
-    //     .then(() => {
-    //       this.getMessagingToken();
-    //     })
-    //     .catch(err => {
-    //       console.log("Unable to get permission to notify.", err);
-    //     });
-    // },
-    // listenTokenRefresh() {
-    //   const currentMessageToken = window.localStorage.getItem("messagingToken");
-    //   console.log("currentMessageToken", currentMessageToken);
-    //   if (!!currentMessageToken) {
-    //     messaging.onTokenRefresh(function() {
-    //       messaging
-    //         .getToken()
-    //         .then(function(token) {
-    //           this.saveToken(token);
-    //         })
-    //         .catch(function(err) {
-    //           console.log("Unable to retrieve refreshed token ", err);
-    //         });
-    //     });
-    //   }
-    // },
-    // saveToken(token) {
-    //   console.log("tokens", token);
-    //   axios
-    //     .post(
-    //       "https://us-central1-bmwgunicode.cloudfunctions.net/GeneralSubscription",
-    //       { token }
-    //     )
-    //     .then(response => {
-    //       window.localStorage.setItem("messagingToken", token);
-    //       console.log(response);
-    //     })
-    //     .catch(err => {
-    //       console.log(err);
-    //     });
-    // },
-    // logout: function() {
-    //   firebase.auth.signOut().then(() => {
-    //     this.$router.replace("login");
-    //   });
-    // }
   }
 };
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
+</style>
