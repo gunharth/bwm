a2c490906a840b144789322d0409defccee9753a
Gunharth Randolf <hello@gunharth.io>
2019-06-27 22:28:39 +0200

    Basic leaflet map

--- src/main.js
+++ src/main.js
@@ -7,6 +7,17 @@ import "./registerServiceWorker";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 import store from "./store/store";
 
+// leaflet
+import { Icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+// this part resolve an issue where the markers would not appear
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
+
 Vue.config.productionTip = false;
 
 let app = "";
--- src/router.js
+++ src/router.js
@@ -33,6 +33,14 @@ const router = new Router({
       meta: {
         requiresAuth: true
       }
+    },
+    {
+      path: "/map",
+      name: "map",
+      component: () => import("./components/Map.vue"),
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 });
